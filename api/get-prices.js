235547569179319ab8d990b55a284ad9b0a7e89c
fajuda0-ship-it <fajuda0@gmail.com>
2026-03-07 21:06:18 +0300

    Update get-prices.js

--- api/get-prices.js
+++ api/get-prices.js
@@ -1,5 +1,13 @@
 export default async function handler(req, res) {
-  const response = await fetch('https://api.simcotools.com/v1/realms/0/market/prices');
-  const data = await response.json();
-  res.status(200).json(data);
+  try {
+    const response = await fetch('https://api.simcotools.com/v1/realms/0/market/prices', {
+      headers: { 'Cache-Control': 'no-cache' }
+    });
+    const data = await response.json();
+    // Tarayıcının itiraz etmemesi için kapıyı sonuna kadar açıyoruz
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Veri çekilemedi' });
+  }
 }
