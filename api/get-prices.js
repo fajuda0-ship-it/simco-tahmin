f43d76d28dc0ff239656f6810e2a73e7eb3039ac
fajuda0-ship-it <fajuda0@gmail.com>
2026-03-07 21:42:45 +0300

    Update get-prices.js

--- api/get-prices.js
+++ api/get-prices.js
@@ -1,27 +1,14 @@
 export default async function handler(req, res) {
-  try {
-    const response = await fetch('https://api.simcotools.com/v1/realms/0/market/prices', {
-      headers: { 'Cache-Control': 'no-cache' },
-      next: { revalidate: 0 } // Vercel için önbelleği kapatır
-    });
-    
-    const allData = await response.json();
 
-    // DİKKAT: Robotu (114) daha buradayken buluyoruz!
-    // Böylece GitHub tarafında hiçbir işlem yapmana gerek kalmaz.
-    const robot = allData.find(item => item.resourceId === 114 && item.quality === 0);
+const response = await fetch("https://api.simcotools.com/v1/realms/0/market/prices");
+const data = await response.json();
 
-    // Kapıyı açıyoruz (CORS)
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
+const item = data.find(
+i => i.resourceId === 114 && i.quality === 0
+);
 
-    if (robot) {
-      res.status(200).json({ price: robot.price });
-    } else {
-      res.status(404).json({ error: 'Robot 114 bulunamadı' });
-    }
+res.status(200).json({
+price: item ? item.price : null
+});
 
-  } catch (error) {
-    res.status(500).json({ error: 'Simco Baglanti Hatasi' });
-  }
 }
